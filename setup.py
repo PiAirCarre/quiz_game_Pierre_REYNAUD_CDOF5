2bf1570c4ea96030ee26fdcebb63aa8a3fcc78cc
PiAirCarre <159248209+PiAirCarre@users.noreply.github.com>
2025-01-14 14:10:18 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+  name='{quiz_game_Pierre_REYNAUD_CDOF5}',
+  version='1.0',
+  author='{PiAirCarre}',
+  license='MIT',
+  long_description=open('README.md').read(),
+)
